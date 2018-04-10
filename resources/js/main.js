2532761c97f374d89bba7025757769d8c6a74d66
comilgame <duvercitocando@gmail.com>
2018-04-10 18:06:09 -0500

    agregado el javascript

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+nombreDelJuego = "Comil Game";
+edadRequerida = 3;
+edadDelJugador = 15;
+nombreDelJugador = "Daniel Andrade";
+
+if (edadDelJugador > edadRequerida) {
+	alert("Hola! Bienvenido")
+} else{
+	alert("Lo siento, no alcanzas la edad minima para jugar")
+}
