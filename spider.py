2291bcd66f44ba5e8e2ddfd42963900a251887a6
comilgame <duvercitocando@gmail.com>
2018-04-23 17:35:49 -0500

    guardar

---
+++ spider.py
@@ -0,0 +1,10 @@
+__author__ = 'DUVER'
+
+from scrapy.item import Field
+from scrapy.item import Item
+from scrapy.spiders import Spider 
+from scrapy.selector import Selector
+from scrapy.contrib.loader import Itemloader
+
+
+#Probrela: Extraer informacion de StackOverflow: Preguntas
